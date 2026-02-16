345b7bbe80284b53a1c4419319abd9e93716ea52
mma2367 <mma2367@g.rit.edu>
2026-02-16 20:48:09 +0400

    Initial Spring Boot scaffold

---
+++ src/test/java/org/example/spiritassignment1group4/SpiRitAssignment1Group4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spiritassignment1group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiRitAssignment1Group4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
