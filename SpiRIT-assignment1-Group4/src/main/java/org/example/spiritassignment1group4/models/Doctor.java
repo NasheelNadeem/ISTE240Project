890cdb9a27c75a1d083d11eda1c38ca4fb1164d2
mma2367 <mma2367@g.rit.edu>
2026-02-18 17:55:51 +0400

    Adding the doctor bean

---
+++ SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/models/Doctor.java
@@ -0,0 +1,49 @@
+package org.example.spiritassignment1group4;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Doctor {
+    private String name;
+    private String doctorID;
+    private String profession;
+
+    public Doctor(String name, String doctorID, String profession) {
+        this.name = name;
+        this.doctorID = doctorID;
+        this.profession = profession;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "name='" + name + '\'' +
+                ", doctorID='" + doctorID + '\'' +
+                ", profession='" + profession + '\'' +
+                '}';
+    }
+}
