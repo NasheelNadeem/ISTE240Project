72017833a3c6c7a7a64f90ff42dbc864c8d452d5
maa5277 <127394659+maa5277@users.noreply.github.com>
2026-04-24 12:07:26 +0400

    Update SpiRitAssignment1Group4Application.java

--- SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/SpiRitAssignment1Group4Application.java
+++ SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/SpiRitAssignment1Group4Application.java
@@ -1,7 +1,11 @@
 package org.example.spiritassignment1group4;
 
+import org.example.spiritassignment1group4.models.*;
+import org.example.spiritassignment1group4.repositories.FeedbackRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class SpiRitAssignment1Group4Application {
@@ -10,4 +14,24 @@ public class SpiRitAssignment1Group4Application {
         SpringApplication.run(SpiRitAssignment1Group4Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner seed(FeedbackRepository repo){
+        return args -> {
+
+            Feedback f = new Feedback();
+
+            Patient p = new Patient();
+            p.setName("Mark");
+
+            Doctor d = new Doctor();
+            d.setName("Habil");
+
+            f.setPatient(p);
+            f.setDoctor(d);
+            f.setRating(5);
+            f.setComment("Great service");
+
+            repo.save(f);
+        };
+    }
 }
