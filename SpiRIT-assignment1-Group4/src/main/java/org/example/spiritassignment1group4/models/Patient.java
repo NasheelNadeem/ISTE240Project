e44eea6631e7ff174de70e0fad810dfe8887f982
mma2367 <mma2367@g.rit.edu>
2026-04-26 23:57:34 +0400

    Pushing Ryan's works.

--- SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/models/Patient.java
+++ SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/models/Patient.java
@@ -1,18 +1,34 @@
+/*
+ * Student Name: Ryan Benson
+ * UID: 421007591
+ */
 package org.example.spiritassignment1group4.models;
 
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "patients")
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
 
-    public Long getPatientID() {
+    public Patient() {}
+
+    public Patient(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
