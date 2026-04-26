fa12d2d9bfe6db88f424f87aa460cb4dcf787a7c
mma2367 <mma2367@g.rit.edu>
2026-04-26 23:54:09 +0400

    Pushing Mohamed Ahmed works.

--- SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/services/FeedbackServices.java
+++ SpiRIT-assignment1-Group4/src/main/java/org/example/spiritassignment1group4/services/FeedbackServices.java
@@ -1,61 +1,56 @@
+// Mohamed Ahmed 767000238
+
 package org.example.spiritassignment1group4.services;
 
-import org.example.spiritassignment1group4.models.Doctor;
+import jakarta.transaction.Transactional;
 import org.example.spiritassignment1group4.models.Feedback;
-import org.example.spiritassignment1group4.models.Patient;
+import org.example.spiritassignment1group4.repositories.FeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class FeedbackServices {
-    List<Feedback> feedbacks = new ArrayList<>();
 
-    public FeedbackServices() {
-        var p1 = new Patient();
-        p1.setName("Mark House");
-        p1.setPatientId(11);
+    @Autowired
+    private FeedbackRepository feedbackRepository;
 
-        var d1 = new Doctor();
-        d1.setName("Habil Gray");
-        d1.setDoctorID(55);
-
-        var f1 = new Feedback();
-        f1.setPatient(p1);
-        f1.setDoctor(d1);
-        f1.setRating(5);
-        f1.setComment("Very helpful session.");
-
-        feedbacks.add(f1);
-
-        var p2 = new Patient();
-        p2.setName("John Jalp");
-        p2.setPatientId(12);
-
-        var d2 = new Doctor();
-        d2.setName("Pasor Serical");
-        d2.setDoctorID(52);
-
-        var f2 = new Feedback();
-        f2.setPatient(p2);
-        f2.setDoctor(d2);
-        f2.setRating(4);
-        f2.setComment("Good support and advice.");
-
-        feedbacks.add(f2);
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackRepository.findAll();
     }
 
-    public List<Feedback> findAllFeedbacks() {
-        return this.feedbacks;
+    public Optional<Feedback> getFeedbackById(Long id) {
+        return feedbackRepository.findById(id);
     }
 
-    public void saveFeedback(Patient patient, Doctor doctor, int rating, String comment) {
-        var newFeedback = new Feedback();
-        newFeedback.setPatient(patient);
-        newFeedback.setDoctor(doctor);
-        newFeedback.setRating(rating);
-        newFeedback.setComment(comment);
-        this.feedbacks.add(newFeedback);
+    public List<Feedback> findByRating(int rating) {
+        return feedbackRepository.findByRating(rating);
+    }
+
+    public List<Feedback> findByCommentContaining(String keyword) {
+        return feedbackRepository.findByCommentContaining(keyword);
+    }
+
+    public List<Feedback> findByDoctorName(String name) {
+        return feedbackRepository.findByDoctorName(name);
+    }
+
+    public List<Feedback> findByPatientName(String name) {
+        return feedbackRepository.findByPatientName(name);
+    }
+
+    public Feedback saveFeedback(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    public void deleteFeedback(Long id) {
+        feedbackRepository.deleteById(id);
+    }
+
+    public void updateCommentById(Long id, String comment) {
+        feedbackRepository.updateCommentById(id, comment);
     }
 }
